d9e7b937cd3649082f7475287ba424ff6217d216
Umair Riaz <49087501+umairriaz789@users.noreply.github.com>
2023-06-21 16:07:00 +0500

    updated

--- src/components/Dashboard/withdrawStke.jsx
+++ src/components/Dashboard/withdrawStke.jsx
@@ -1,187 +1,160 @@
 import React, { useEffect, useState } from "react";
 import { FormActionButton } from "../buttons/form_action_button";
-import { Table } from "../table";
 import { Body, H4, P } from "../typography";
-import { Info } from "react-feather";
-// import { COLORS } from "src/assets/styles/theme";
-import { CInput } from "../inputs/cinput";
 import { Link, useLocation } from "react-router-dom";
 import { Chrysus } from "../../assets";
 import styled from "styled-components";
 import Utils from "../../utilities";
 import { ethers } from "ethers";
-import chrysus from "../../abis/Chrysus.json";
-import { CHRYSUS } from "../../constant.js";
-import { DAI, ETH } from "../../constant";
+import { DAI, ETH,MockStabilityModule } from "../../constant";
+import { StakeABI } from "../../abis/MockStabilityModule";
 
 export const WithdrawStake = () => {
-  const [TotalStake, setTotalStake] = useState(0);
-  const addrees = localStorage.getItem("accounts");
-  // const location = useLocation()
-  // const { collateral } = location.state;
-  const [amount, setAmount] = useState(0);
-  const [loading, setLoading] = useState(false);
+    const [TotalStake, setTotalStake] = useState(0);
+    const addrees = localStorage.getItem("accounts");
+    // const location = useLocation()
+    // const { collateral } = location.state;
+    const [amount, setAmount] = useState(0);
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchTotalStake = async () => {
-      try {
-        const { ethereum } = window;
-        if (ethereum) {
-          let chainId = await ethereum.request({ method: "eth_chainId" });
-          console.log("Connecteds to chains " + chainId);
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const _signer = provider.getSigner();
-          const contract = new ethers.Contract(
-            MockStabilityModule,
-            StakeABI,
-            _signer
-          );
-          let Stake = await contract.getTotalPoolAmount();
+    useEffect(() => {
+        const fetchTotalStake = async () => {
+            try {
+                const { ethereum } = window;
+                if (ethereum) {
+                    let chainId = await ethereum.request({ method: "eth_chainId" });
+                    console.log("Connecteds to chains " + chainId);
+                    const provider = new ethers.providers.Web3Provider(ethereum);
+                    const _signer = provider.getSigner();
+                    const contract = new ethers.Contract(
+                        MockStabilityModule,
+                        StakeABI,
+                        _signer
+                    );
+                    let Stake = await contract.getTotalPoolAmount();
+                    !TotalStake & setTotalStake((Number(Stake)).toFixed(2));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchTotalStake()
+    },[ethereum]);
+    
+
+    const WithdrawStakes = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (ethereum) {
+                let chainId = await ethereum.request({ method: "eth_chainId" });
+                console.log("Connecteds to chains " + chainId);
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const _signer = provider.getSigner();
+                const Stakecontract = new ethers.Contract(
+                    MockStabilityModule,
+                    StakeABI,
+                    _signer
+                );
+                setLoading(true);
+                Txn = await Stakecontract.withdrawStake(DAI ,  ethers.utils.parseUnits(String(amount)));
+                await Txn.wait();
+                setLoading(false);
+                console.log('Withdraw stake successfully!');
+                // window.location.reload();
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error('Error:', error);
         }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchTotalStake();
-  }, [ethereum]);
-
-  const withdraw = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        let chainId = await ethereum.request({ method: "eth_chainId" });
-        console.log("Connecteds to chains " + chainId);
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const _signer = provider.getSigner();
-        const chrysusContract = new ethers.Contract(
-          CHRYSUS,
-          chrysus.abi,
-          _signer
-        );
-        // const Stakecontract = new ethers.Contract(
-        //     MockStabilityModule,
-        //     StakeABI,
-        //     _signer
-        // );
-
-        // const _collateral = collateral == "DAI" ? DAI : ETH;
-
-        let Txn = await chrysusContract.withdrawCollateral(
-          _collateral,
-          ethers.utils.parseUnits(String(amount))
-        );
-        setLoading(true);
-        await Txn.wait();
-        setLoading(false);
-        console.log("Withdrawn successfully!");
-        window.location.reload();
-      }
-    } catch (error) {
-      console.error("Error:", error);
     }
-  };
 
-  return (
-    <Section>
-      <div className="row w-100" style={{ borderRadius: "16px" }}>
-        <div className="col ">
-          <div
-            className="w-100 d-flex flex-column align-items-center text-center"
-            style={{
-              backgroundColor: "#211f21",
-              borderRadius: "16px",
-              color: "#846424",
-            }}
-          >
-            <div className="mt-5" />
-            <H4>Withdraw</H4>
-            <div className="d-flex flex-column align-items-start">
-              <P className="m-0">How much would you like to Withdraw?</P>
-              <Body className="m-0">
-                Please Enter the amount you would like to Withdraw
-              </Body>
-              <div className="my-3" />
-              <label className="form-label text-primary">
-                Avaliable to Withdraw : {}
-                {Utils.toFixedNoRounding(TotalStake, 3)}{" "}
-              </label>
-              <div
-                className="input-group"
-                style={{
-                  backgroundColor: "#1A1917",
-                  color: "#846424",
-                }}
-              >
-                <input
-                  type="text"
-                  className="form-control"
-                  style={{
-                    backgroundColor: "#1A1917",
-                    color: "#846424",
-                  }}
-                  // onChange={(e) => setAmount(e.target.value)}
-                  placeholder="0.00"
-                />
-                <span
-                  style={{
-                    backgroundColor: "#1A1917",
-                    color: "#846424",
-                  }}
-                  className="input-group-text"
-                >
-                  <img loading="lazy" src={Chrysus} alt="meta" />
-                </span>
-              </div>
-              <div className="my-1" />
+    return (
+        <Section>
+            <div className="row w-100" style={{ borderRadius: "16px" }}>
+                <div className="col ">
+                    <div
+                        className="w-100 d-flex flex-column align-items-center text-center"
+
+                        style={{
+                            backgroundColor: "#211f21",
+                            borderRadius: "16px",
+                            color: "#846424",
+                        }}
+                    >
+                        <div className="mt-5"></div>
+                        <H4>Withdraw</H4>
+                        <div className="d-flex flex-column align-items-start">
+                            <P className="m-0">
+                                How much would you like to Withdraw?
+                            </P>
+                            <Body className="m-0">
+                                Please Enter the amount you would like to Withdraw
+                            </Body>
+                            <div className="my-3"></div>
+                            <label className="form-label text-primary">Avaliable to Withdraw : { }
+                            {Utils.toFixedNoRounding(TotalStake, 3)} </label>
+                            <div className="input-group" style={{
+                                backgroundColor: "#1A1917",
+                                color: "#846424",
+                            }}>
+                                <input type="text" className="form-control"
+                                    style={{
+                                        backgroundColor: "#1A1917",
+                                        color: "#846424",
+                                    }}
+                                    onChange={(e) => setAmount(e.target.value)}
+                                    placeholder="0.00" />
+                                <span style={{
+                                    backgroundColor: "#1A1917",
+                                    color: "#846424",
+                                }} className="input-group-text"><img loading="lazy" src={Chrysus} alt="meta" /></span>
+                            </div>
+                            <div className="my-1"></div>
+                           
+                        </div>
+                        <div
+                            className="w-100"
+                            style={{ borderTop: "1px solid rgba(255, 255, 255, 0.1)" }}
+                        ></div>
+                        <div className="w-100 d-flex flex-row justify-content-start p-3">
+                            {/* Form Actions */}
+                            <Link to={"/accounts/governance"}>
+                                <FormActionButton color="white" outline={true} onClick={() => console.log("first")}>
+                                    Back
+                                </FormActionButton>
+                            </Link>
+                            <button
+                                style={{
+                                    borderRadius: "40px",
+                                    background: "linear-gradient(270deg, #EDC452 0.26%, #846424 99.99%, #846424 100%), #846424",
+                                    // Fonts
+                                    fontStyle: "normal",
+                                    padding: "10px",
+                                    fontWeight: "700",
+                                    fontSize: "14px",
+                                    lineHeight: "24px",
+                                    letterSpacing: "1px",
+                                    textTransform: "uppercase",
+                                    color: "black",
+                                }}
+                                onClick={() => WithdrawStakes()}
+                            >
+                                {loading ? "Processing...." : "Continue"}
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div
-              className="w-100"
-              style={{ borderTop: "1px solid rgba(255, 255, 255, 0.1)" }}
-            />
-            <div className="w-100 d-flex flex-row justify-content-start p-3">
-              {/* Form Actions */}
-              <Link to={"/accounts/governance"}>
-                <FormActionButton
-                  color="white"
-                  outline
-                  onClick={() => console.log("first")}
-                >
-                  Back
-                </FormActionButton>
-              </Link>
-              <button
-                style={{
-                  borderRadius: "40px",
-                  background:
-                    "linear-gradient(270deg, #EDC452 0.26%, #846424 99.99%, #846424 100%), #846424",
-                  // Fonts
-                  fontStyle: "normal",
-                  padding: "10px",
-                  fontWeight: "700",
-                  fontSize: "14px",
-                  lineHeight: "24px",
-                  letterSpacing: "1px",
-                  textTransform: "uppercase",
-                  color: "black",
-                }}
-                onClick={() => withdraw()}
-              >
-                {loading ? "Processing...." : "Continue"}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Section>
-  );
+        </Section>
+    );
 };
 
 const Section = styled.section`
   margin-left: 18vw;
   padding: 2rem;
   height: 100%;
-  background-color: #121212;
+  background-color:  #121212;
   .grid {
     display: flex;
     flex-direction: column;
