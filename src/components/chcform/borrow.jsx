e999de1dc13f551c9180fde31a84fab56c747960
Umair Riaz <49087501+umairriaz789@users.noreply.github.com>
2023-07-20 11:18:40 +0500

    updated

--- src/components/chcform/borrow.jsx
+++ src/components/chcform/borrow.jsx
@@ -120,6 +120,26 @@ export const Borrow = () => {
       borrow_value: dai_lend,
     },
   ];
+  const Bat = [
+    {
+      Collateral: "CHC/BAT",
+    }]
+  const Usdt = [
+    {
+      Collateral: "CHC/USDT",
+    }]
+  const Usdc = [
+    {
+      Collateral: "CHC/USDC",
+    }]
+  const Uni = [
+    {
+      Collateral: "CHC/UNI",
+    }]
+  const Wbtc = [
+    {
+      Collateral: "CHC/WBTC",
+    }]
 
   return (
     <>
@@ -270,6 +290,271 @@ export const Borrow = () => {
                                       to={"/accounts/loan/repay"}
                                       state={{ collateral: "DAI" }}
                                     >
+                                      <span
+                                        className="badge cursor-pointer ml-2"
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "black",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          background:
+                                            "linear-gradient(270deg, #EDC452 0.26%, #846424 99.99%, #846424 100%), #846424",
+                                          borderRadius: "40px",
+                                        }}
+                                      >
+                                        Repay
+                                      </span>
+                                    </Link>
+                                  </td>
+                                </tr>
+                              ))}
+                              {Bat.map((item, index) => (
+                                <tr key={index}>
+                                  <td>{item.Collateral}</td>
+                                  <td>0</td>
+                                  <td>$0</td>
+                                  <td>0</td>
+                                  <td>
+                                    <Link
+                                      to={"/accounts/wthdrawLending"}
+                                      state={{ collateral: "ETH" }}
+                                    >
+                                      <span
+                                        className="badge cursor-pointer"
+                                        // onClick={() => setModalShowDAI(true)}
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "#846424",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          backgroundColor: "#1A1917",
+                                          borderRadius: "16px",
+                                          border: "1px solid transparent",
+                                          borderColor: "#846424",
+                                        }}
+                                      >
+                                        Borrow
+                                      </span>
+                                    </Link>
+                                    <Link
+                                      to={"/accounts/lend"}
+                                      state={{ collateral: "ETH" }}
+                                    >
+                                      <span
+                                        className="badge cursor-pointer ml-2"
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "black",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          background:
+                                            "linear-gradient(270deg, #EDC452 0.26%, #846424 99.99%, #846424 100%), #846424",
+                                          borderRadius: "40px",
+                                        }}
+                                      >
+                                        Repay
+                                      </span>
+                                    </Link>
+                                  </td>
+                                </tr>
+                              ))}
+                              {Usdt.map((item, index) => (
+                                <tr key={index}>
+                                  <td>{item.Collateral}</td>
+                                  <td>0</td>
+                                  <td>$0</td>
+                                  <td>0</td>
+                                  <td>
+                                    <Link>
+                                      <span
+                                        className="badge cursor-pointer"
+                                        // onClick={() => setModalShowDAI(true)}
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "#846424",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          backgroundColor: "#1A1917",
+                                          borderRadius: "16px",
+                                          border: "1px solid transparent",
+                                          borderColor: "#846424",
+                                        }}
+                                      >
+                                        Borrow
+                                      </span>
+                                    </Link>
+                                    <Link
+                                      to={"/accounts/lend"}
+                                      state={{ collateral: "ETH" }}
+                                    >
+                                      <span
+                                        className="badge cursor-pointer ml-2"
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "black",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          background:
+                                            "linear-gradient(270deg, #EDC452 0.26%, #846424 99.99%, #846424 100%), #846424",
+                                          borderRadius: "40px",
+                                        }}
+                                      >
+                                        Repay
+                                      </span>
+                                    </Link>
+                                  </td>
+                                </tr>
+                              ))}
+                              {Usdc.map((item, index) => (
+                                <tr key={index}>
+                                  <td>{item.Collateral}</td>
+                                  <td>0</td>
+                                  <td>$0</td>
+                                  <td>0</td>
+                                  <td>
+                                    <Link>
+                                      <span
+                                        className="badge cursor-pointer"
+                                        // onClick={() => setModalShowDAI(true)}
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "#846424",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          backgroundColor: "#1A1917",
+                                          borderRadius: "16px",
+                                          border: "1px solid transparent",
+                                          borderColor: "#846424",
+                                        }}
+                                      >
+                                        Borrow
+                                      </span>
+                                    </Link>
+                                    <Link
+                                      to={"/accounts/lend"}
+                                      state={{ collateral: "ETH" }}
+                                    >
+                                      <span
+                                        className="badge cursor-pointer ml-2"
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "black",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          background:
+                                            "linear-gradient(270deg, #EDC452 0.26%, #846424 99.99%, #846424 100%), #846424",
+                                          borderRadius: "40px",
+                                        }}
+                                      >
+                                        Repay
+                                      </span>
+                                    </Link>
+                                  </td>
+                                </tr>
+                              ))}
+                              {Wbtc.map((item, index) => (
+                                <tr key={index}>
+                                  <td>{item.Collateral}</td>
+                                  <td>0</td>
+                                  <td>$0</td>
+                                  <td>0</td>
+                                  <td>
+                                    <Link>
+                                      <span
+                                        className="badge cursor-pointer"
+                                        // onClick={() => setModalShowDAI(true)}
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "#846424",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          backgroundColor: "#1A1917",
+                                          borderRadius: "16px",
+                                          border: "1px solid transparent",
+                                          borderColor: "#846424",
+                                        }}
+                                      >
+                                        Borrow
+                                      </span>
+                                    </Link>
+                                    <Link
+                                      to={"/accounts/lend"}
+                                      state={{ collateral: "ETH" }}
+                                    >
+                                      <span
+                                        className="badge cursor-pointer ml-2"
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "black",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          background:
+                                            "linear-gradient(270deg, #EDC452 0.26%, #846424 99.99%, #846424 100%), #846424",
+                                          borderRadius: "40px",
+                                        }}
+                                      >
+                                        Repay
+                                      </span>
+                                    </Link>
+                                  </td>
+                                </tr>
+                              ))}
+                              {Uni.map((item, index) => (
+                                <tr key={index}>
+                                  <td>{item.Collateral}</td>
+                                  <td>0</td>
+                                  <td>$0</td>
+                                  <td>0</td>
+                                  <td>
+                                    <Link>
+                                      <span
+                                        className="badge cursor-pointer"
+                                        // onClick={() => setModalShowDAI(true)}
+                                        style={{
+                                          height: "22px",
+                                          width: "80px",
+                                          color: "#846424",
+                                          textTransform: "uppercase",
+                                          fontStyle: "normal",
+                                          fontWeight: "700",
+                                          fontSize: "10px",
+                                          backgroundColor: "#1A1917",
+                                          borderRadius: "16px",
+                                          border: "1px solid transparent",
+                                          borderColor: "#846424",
+                                        }}
+                                      >
+                                        Borrow
+                                      </span>
+                                    </Link>
+                                    <Link>
                                       <span
                                         className="badge cursor-pointer ml-2"
                                         style={{
